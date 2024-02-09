49d4f1cf190ea5c135843be4c9c46667bf8057c1
Sergey_Shein <toxicmoos6.com>
2024-02-09 14:59:10 +0100

    update

--- src/test/java/com/demowebshop/tests/CreateAccountTests.java
+++ src/test/java/com/demowebshop/tests/CreateAccountTests.java
@@ -1,20 +1,19 @@
 package com.demowebshop.tests;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class CreateAccountTests extends TestBase {
+    @BeforeMethod
+    public void ensurePrecondition(){
+
+    }
     @Test
     public void createNewAccountPositiveTest(){
-        //click on Register link
         clickOnRegisterLink();
-        //gender
         clickOnGenderCheckbox();
-        //enter first name
         fillCreateAccountForm("rambo_qa@gmail.com", "PassWord$");
-        //click on Register button
         clickOnRegisterButton();
-        //assert sign out button in present
-
         //Assert.assertTrue(isElementPresent(By.cssSelector("[href='/logout']")));
     }
 
